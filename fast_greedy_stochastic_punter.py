b33642eecbbfbb762735a9aeeacce696847bd9a5
Den Raskovalov <denplusplus@fb.com>
2017-08-05 19:29:13 -0700

    fix

--- fast_greedy_stochastic_punter.py
+++ fast_greedy_stochastic_punter.py
@@ -160,7 +160,7 @@ class FastGreedyStochasticPunter:
                     n_transactions = 0
                     j = 0
                     while j < stochastic_edges:
-                        while (time.clock() - begin)*len(all_edges) > 0.7:
+                        if (time.clock() - begin)*len(all_edges) > 0.7:
                             break
                         if 0 != self.components.num_edges():
                             random_edge = self.components.random_edge()
