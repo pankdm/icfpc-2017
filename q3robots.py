99b88aeaf4e59e3511800c6942a61617006e84d2
Vlad Belous <vladislav@fb.com>
2017-08-06 03:25:47 -0700

    update arena

--- q3robots.py
+++ q3robots.py
@@ -7,6 +7,6 @@ def create_all_robots():
         create_punter(FastGreedyPunter, name="FastGreedyPunter"),
         create_punter(FastGreedyStochasticPunter, name="FastGreedyStochasticPunter"),
         create_punter(FastGreedyStochasticBridgesMaxPunter, name="FastGreedyStochasticBridgesMaxPunter"),
-        create_punter(VladSolver1, name="Vlad-MCTS-15", timeout=0.9, search_width=15),
         create_punter(VladSolver1, name="Vlad-MCTS-15m", timeout=0.9, search_width=15, magic_moves=True),
+        create_punter(VladSolver2, name="Vlad-MCTS-2g", timeout=0.9, search_width=15, magic_moves=True, greedy_threshold=50),
     ]
