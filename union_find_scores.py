a9da1f3cf8491b62593ef095c5308a3096dba915
Den Raskovalov <denplusplus@fb.com>
2017-08-05 18:11:36 -0700

    stochastic

--- union_find_scores.py
+++ union_find_scores.py
@@ -1,3 +1,5 @@
+import random
+
 class ComponentsListWithScores:
     def __init__(self, n, mines, distances):
         self.lists_ = []
@@ -8,11 +10,19 @@ class ComponentsListWithScores:
             self.vertices_.append(i)
         self.transactions_ = []
         self.transctions_scores_ = []
+        self.transctions_edges_ = []
         self.score_ = 0
         self.mines_ = mines
         self.distances_ = distances
+        self.edges_ = set()
 
     def union(self, i, j):
+        if i > j:
+            i, j = j, i
+        self.transctions_edges_[-1].append( (i, j) )
+        seld.edges_.remove( (i, j) )
+        i = self.component(i)
+        j = self.component(j)
         for x in self.lists_[i]:
             if x in self.mines_:
                 for y in self.lists_[j]:
@@ -39,6 +49,7 @@ class ComponentsListWithScores:
     def start_transaction(self):
         self.transactions_.append([])
         self.transctions_scores_.append(self.score_)
+        self.transctions_edges_.append([])
 
     def rollback_transaction(self):
         for t in self.transactions_[-1]:
@@ -59,6 +70,11 @@ class ComponentsListWithScores:
 
     def score(self):
         return self.score_
+
+    def add_egde(self, s, t):
+        if s > t:
+            s, t = t, s
+        self.edges_.insert( (s, t) )
 
 if __name__ == "__main__":
     cl = ComponentsList(5)
