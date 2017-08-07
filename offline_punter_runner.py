82ba46331779b41ceec074068c7e7899c606807a
Den Raskovalov <denplusplus@fb.com>
2017-08-06 23:56:08 -0700

    log exception

--- offline_punter_runner.py
+++ offline_punter_runner.py
@@ -39,10 +39,12 @@ def log_traceback(ex, ex_traceback=None):
     if ex_traceback is None:
         ex_traceback = ex.__traceback__
     tb_lines = [ line.rstrip('\n') for line in traceback.format_exception(ex.__class__, ex, ex_traceback)]
-    exception_logger.log(tb_lines)
+    if log:
+        print >>fLog, tb_lines
 
 if __name__ == "__main__":
     try:
+        raise Exception("fdsfs")
         punter = create_punter(FastGreedyStochasticBridgesVerticesMaxPunter, log=False, name="FastGreedyStochasticBridgesVerticesMaxPunter 1")
         hs = punter.get_handshake()
         writeOutputJson(hs)
@@ -50,9 +52,9 @@ if __name__ == "__main__":
         inp = readInputJson()
         out = punter.run(inp)
         writeOutputJson(out)
-    except ex:
+    except Exception as ex:
         if log:
-            print >>fLog, str(ex)
+            print >>fLog, "Exception:", str(ex)
             _, _, ex_traceback = sys.exc_info()
             log_traceback(ex, ex_traceback)
         sys.exit(1)
