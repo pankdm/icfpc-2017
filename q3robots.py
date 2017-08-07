68e47370d0666f9c7fb5f47d3dd79af807693842
Vlad Belous <vladislav@fb.com>
2017-08-06 22:07:03 -0700

    make 2i non-greedy

--- q3robots.py
+++ q3robots.py
@@ -7,9 +7,9 @@ def create_all_robots():
         create_punter(FastGreedyPunter, name="FastGreedyPunter"),
         create_punter(FastGreedyStochasticPunter, name="FastGreedyStochasticPunter"),
         create_punter(FastGreedyStochasticBridgesMaxPunter, name="FastGreedyStochasticBridgesMaxPunter"),
-        create_punter(VladSolver2, name="Vlad-MCTS-2i", timeout=0.9, search_width=15, magic_moves=True, greedy_threshold=50),
-        create_punter(VladSolver3, name="Vlad-MG-3", timeout=0.9),
-        create_punter(VladSolver4, name="Vlad-MG-4", timeout=0.9),
+        create_punter(VladSolver2, name="Vlad-MCTS-2i", timeout=0.8, search_width=15, magic_moves=True, greedy_threshold=9999, playout_max_depth=50),
+        create_punter(VladSolver3, name="Vlad-MG-3", timeout=0.8),
+        create_punter(VladSolver4, name="Vlad-MG-4", timeout=0.8),
         create_punter(FastGreedyStochasticBridgesVerticesMaxPunter, name="FastGreedyStochasticBridgesVerticesMaxPunter"),
         # create_punter(UberFastGreedyStochasticPunter, name="UberFastGreedyStochasticPunter"),
     ]
