5010041132dcbcbcd567a5af36687e1c6efe2227
Den Raskovalov <denplusplus@fb.com>
2017-08-06 23:51:07 -0700

    log exceptions

--- offline_punter_runner.py
+++ offline_punter_runner.py
@@ -33,11 +33,26 @@ def writeOutputJson(obj):
     sys.stdout.write(res)
     sys.stdout.flush()
 
+import traceback
+
+def log_traceback(ex, ex_traceback=None):
+    if ex_traceback is None:
+        ex_traceback = ex.__traceback__
+    tb_lines = [ line.rstrip('\n') for line in traceback.format_exception(ex.__class__, ex, ex_traceback)]
+    exception_logger.log(tb_lines)
+
 if __name__ == "__main__":
-    punter = create_punter(FastGreedyStochasticBridgesVerticesMaxPunter, log=False, name="FastGreedyStochasticBridgesVerticesMaxPunter 1")
-    hs = punter.get_handshake()
-    writeOutputJson(hs)
-    ack = readInputJson()
-    inp = readInputJson()
-    out = punter.run(inp)
-    writeOutputJson(out)
+    try:
+        punter = create_punter(FastGreedyStochasticBridgesVerticesMaxPunter, log=False, name="FastGreedyStochasticBridgesVerticesMaxPunter 1")
+        hs = punter.get_handshake()
+        writeOutputJson(hs)
+        ack = readInputJson()
+        inp = readInputJson()
+        out = punter.run(inp)
+        writeOutputJson(out)
+    except ex:
+        if log:
+            print >>fLog, str(ex)
+            _, _, ex_traceback = sys.exc_info()
+            log_traceback(ex, ex_traceback)
+        sys.exit(1)
